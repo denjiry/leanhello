2149abc355f575a931b69f4b3941dca1a7c0c14b
denjiry <k.denjiry@gmail.com>
2022-09-13 04:05:09 +0900

    update

--- Leanhello.lean
+++ Leanhello.lean
@@ -1,1 +1,13 @@
 def hello := "world"
+
+axiom A : Type
+axiom Blue : A -> Prop
+def claim: Prop := ∀ (x: A), Blue x
+#check claim
+
+def ja (_s : String) : Prop :=
+  True
+
+#check ja
+
+def claim2 := ja "hello"
